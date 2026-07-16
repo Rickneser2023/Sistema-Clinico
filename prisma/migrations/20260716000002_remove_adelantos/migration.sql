6c9e63e194e90349e8f657916b7f09399da2e8b3
Soviet117 <adixontisg31@gmail.com>
2026-07-16 16:49:48 -0500

    fix: reorder migration SQL to drop columns before enum type

--- prisma/migrations/20260716000002_remove_adelantos/migration.sql
+++ prisma/migrations/20260716000002_remove_adelantos/migration.sql
@@ -1,6 +1,3 @@
--- DropEnum
-DROP TYPE "EstadoAdelanto";
-
 -- AlterTable
 ALTER TABLE "Factura" DROP COLUMN "montoAdelanto",
 DROP COLUMN "metodoAdelanto",
@@ -8,3 +5,6 @@ DROP COLUMN "estadoAdelanto",
 DROP COLUMN "comprobanteUrl",
 DROP COLUMN "fechaComprobante",
 DROP COLUMN "validadoPorId";
+
+-- DropEnum
+DROP TYPE "EstadoAdelanto";
